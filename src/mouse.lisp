82159c351aef4d332625d46577163823c2ade1ec
Mariano Montone <marianomontone@gmail.com>
2021-05-08 13:00:16 -0300

    More docs

--- src/mouse.lisp
+++ src/mouse.lisp
@@ -1,12 +1,18 @@
 (in-package :cl-selenium)
 
 (defun mouse-move-to (x y &key element (session *session*))
+  "Move the mouse by an offset of the specificed element. If no element is specified, the move is relative to the current mouse cursor. If an element is provided but no offset, the mouse will be moved to the center of the element. If the element is not visible, it will be scrolled into view.
+
+See: https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#sessionsessionidmoveto"
   (http-post-check (session-path session "/moveto")
                    (list :element (when element (element-id element))
                          :xoffset x
                          :yoffset y)))
 
 (defun mouse-click (button &key (session *session*))
+  "Click any mouse button (at the coordinates set by the last moveto command). Note that calling this command after calling buttondown and before calling button up (or any out-of-order interactions sequence) will yield undefined behaviour).
+
+See: https://github.com/SeleniumHQ/selenium/wiki/JsonWireProtocol#sessionsessionidclick"
   (http-post-check (session-path session "/click")
                    `(:button ,(ecase button
                                      (:left 0)
