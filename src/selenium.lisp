f8dd3e23c9805313fe605cc181c27b3885457328
Mariano Montone <marianomontone@gmail.com>
2021-05-08 13:22:40 -0300

    Another docs update

--- src/selenium.lisp
+++ src/selenium.lisp
@@ -146,15 +146,7 @@ See: https://www.w3.org/TR/webdriver1/#get-element-text."
   (http-get-value (session-path session "/element/~a/text" (element-id element))))
 
 (defun element-displayed (element &key (session *session*))
-  "Although WebDriver does not define a primitive to ascertain the visibility of an element in the viewport, we acknowledge that it is an important feature for many users. Here we include a recommended approach which will give a simplified approximation of an element’s visibility, but please note that it relies only on tree-traversal, and only covers a subset of visibility checks.
-
-The visibility of an element is guided by what is perceptually visible to the human eye. In this context, an element's displayedness does not relate to the visibility or display style properties.
-
-The approach recommended to implementors to ascertain an element's visibility was originally developed by the Selenium project, and is based on crude approximations about an element's nature and relationship in the tree. An element is in general to be considered visible if any part of it is drawn on the canvas within the boundaries of the viewport.
-
-The element displayed algorithm is a boolean state where true signifies that the element is displayed and false signifies that the element is not displayed. To compute the state on element, invoke the Call(bot.dom.isShown, null, element). If doing so does not produce an error, return the return value from this function call. Otherwise return an error with error code unknown error.
-
-This function is typically exposed to GET requests with a URI Template of /session/{session id}/element/{element id}/displayed.
+  "Returns T if ELEMENT is visible.
 
 See: https://www.w3.org/TR/webdriver1/#element-displayedness."
   (http-get-value (session-path session "/element/~a/displayed" (element-id element))))
