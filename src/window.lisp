0cd0c790d4bea6ab8eb9d03bdcad8bc7468ed4ae
Arnold Noronha <arnold@tdrhq.com>
2021-12-13 10:55:14 -0500

    Add window resize

---
+++ src/window.lisp
@@ -0,0 +1,25 @@
+(in-package :webdriver-client)
+
+(defun set-window-rect (&key x y width height (session *session*))
+  (http-post-check
+   (session-path session "/window/rect")
+   :x x
+   :y y
+   :width width
+   :height height))
+
+(defun get-window-rect (&key (session *session*))
+  (let ((alist (http-get-value
+                (session-path session "/window/rect"))))
+    (list
+     (assoc-value alist :x)
+     (assoc-value alist :y)
+     (assoc-value alist :width)
+     (assoc-value alist :height))))
+
+(defun window-resize (&key width height (session *session*))
+  (destructuring-bind (x y old-width old-height) (get-window-rect :key session)
+    (set-window-rect
+     x y
+     (or width old-width)
+     (or height old-height))))
